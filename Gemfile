1f5f03154886216a3cbcbcaf3c5cac6c39b3613c
Derek Kastner <dkastner@gmail.com>
2010-06-24 09:27:41 -0400

    Setting up env for campfire and Heroku

--- Gemfile
+++ Gemfile
@@ -29,13 +29,13 @@ gem "sinatra-authorization", "1.0.0"
 # = IRC
 # gem "shout-bot"
 # = Campfire
-# gem "broach", :git => "git://github.com/Manfred/broach.git"
-# gem "nap", :git => "git://github.com/qrush/nap.git"
+gem "broach", :git => "git://github.com/Manfred/broach.git"
+gem "nap", :git => "git://github.com/qrush/nap.git"
 
 # = Dependencies for the :dj builder
-# gem "activerecord"
-# gem "sqlite3-ruby"
-# gem "delayed_job", :git => "git://github.com/tobi/delayed_job.git"
+gem "activerecord"
+gem "sqlite3-ruby"
+gem "delayed_job", :git => "git://github.com/tobi/delayed_job.git"
 # = Dependency for the :resque builder
 # gem "resque"
 
--- init.rb
+++ init.rb
@@ -1,22 +1,24 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
 
-require ".bundle/environment"
-require "integrity"
+begin
+  require File.expand_path("../.bundle/environment", __FILE__)
+rescue LoadError
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
 
-# Uncomment as appropriate for the notifier you want to use
-# = Email
-# require "integrity/notifier/email"
-# = IRC
-# require "integrity/notifier/irc"
-# = Campfire
-# require "integrity/notifier/campfire"
+require "integrity"
+require "integrity/notifier/campfire"
 
 Integrity.configure do |c|
-  c.database     "sqlite3:integrity.db"
-  c.directory    "builds"
-  c.base_url     "http://ci.example.org"
-  c.log          "integrity.log"
-  c.github       "SECRET"
+  c.database  ENV["DATABASE_URL"]
+  c.directory "tmp"
+  c.base_url  ENV["URL"]
+  c.log       "tmp/integrity.log"
+  c.github    ENV["GITHUB_TOKEN"] || "TOKEN"
   c.build_all!
   c.builder      :threaded, 5
+  c.user         ENV['HTTP_AUTH_USERNAME']
+  c.pass         ENV['HTTP_AUTH_PASSWORD']
 end
